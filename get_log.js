f2203e4b6b82aa38ddb6c42a5095c8abab8d7712
madhu yadav <iitjeesankalp@gmail.com>
2026-03-25 11:29:38 +0530

    Remove Private Space and clean repository

--- get_log.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-const output = execSync('git log --oneline -n 12').toString();
-const lines = output.split('\n').map(l => l.substring(0, 50));
-console.log(lines.join('\n'));
--- git_log.js
+++
@@ -1,2 +0,0 @@
-const { execSync } = require('child_process');
-console.log(execSync('git log --oneline -n 12').toString());
